1cf658477361ccb9003f03607a71d73900e3795b
5-1 <31527530+5-1@users.noreply.github.com>
2017-11-20 19:56:11 +0100

    files of post system and commentary

---
+++ model.php
@@ -0,0 +1,71 @@
+<?php
+function getPosts()
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=articles;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $req = $db->query('SELECT * FROM articles ORDER BY creation_date DESC');
+
+    return $req;
+}
+
+
+
+function postComment($postId, $author, $comment)
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=articles;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+    $affectedLines = $comments->execute(array($postId, $author, $comment));
+
+    return $affectedLines;
+}
+
+function getPost($postId)
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=articles;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles WHERE id = ?');
+    $req->execute(array($postId));
+    $post = $req->fetch();
+
+    return $post;
+}
+
+function getComments($postId)
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=articles;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+    $comments->execute(array($postId));
+
+    return $comments;
+}
+
